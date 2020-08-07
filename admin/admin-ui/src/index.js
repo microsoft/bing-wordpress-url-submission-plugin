fb771c9a6719eccdfd654135e68c5bbeb0eaf196
Nevin Valsaraj <nevalsar@microsoft.com>
2020-08-07 16:57:59 +0530

    Rename settings page root element
    Rename root element of settings page to avoid conflicts.

--- admin/admin-ui/src/index.js
+++ admin/admin-ui/src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("bw-Root")
 );
 
 // If you want your app to work offline and load faster, you can change
--- admin/partials/bing-url-submission-admin-react.php
+++ admin/partials/bing-url-submission-admin-react.php
@@ -1,7 +1,7 @@
-<div id="root"></div>
+<div id="bw-Root"></div>
 <script>
     (function( $ ) {
-        var bwApp = $('#root');
+        var bwApp = $('#bw-Root');
         if (bwApp) {
             var wp_body = bwApp.parents()[0];
             var update_nags = wp_body.querySelectorAll('.update-nag');
