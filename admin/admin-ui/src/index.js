4928ccbf4303d598ac89da86cb9bed9aae8053ae
Nevin Valsaraj <nevalsar@microsoft.com>
2020-08-07 20:23:25 +0530

    Update root element name
    Update root element name used to load settings page.

--- admin/admin-ui/src/index.js
+++ admin/admin-ui/src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("bw-Root")
+  document.getElementById("bwAppRoot")
 );
 
 // If you want your app to work offline and load faster, you can change
--- admin/partials/bing-url-submission-admin-react.php
+++ admin/partials/bing-url-submission-admin-react.php
@@ -1,7 +1,7 @@
-<div id="bw-Root"></div>
+<div id="bwAppRoot"></div>
 <script>
     (function( $ ) {
-        var bwApp = $('#bw-Root');
+        var bwApp = $('#bwAppRoot');
         if (bwApp) {
             var wp_body = bwApp.parents()[0];
             var update_nags = wp_body.querySelectorAll('.update-nag');
